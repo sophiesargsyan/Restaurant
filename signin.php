d732b22fb966181e53e422b6ddbfac9cb3cfc367
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-09-23 18:27:33 +0400

    created signin.php

---
+++ signin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Sign In | Black and White </title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="images/cropped_logo.png" type="image/x-icon">
+</head>
+<body>
+    
+</body>
+</html>
