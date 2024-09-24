dbdb99aa2be3fc7e71ff9db730dfa742abdb4ca8
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-09-24 16:09:30 +0400

    sign in form

--- signin.php
+++ signin.php
@@ -10,16 +10,37 @@
     <link rel="icon" href="images/cropped_logo.png" type="image/x-icon">
 </head>
 <body>
-    <header>
-    </header>
-    <main>
-
-    </main>
-    <footer>
-
-    </footer>
+    <div class="card">
+        <div class="card-header text-center">
+            <h2>Sign In</h2>
+        </div>
+        <div class="card-body">
+            <form action="login.php" method="POST">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="rememberMe">
+                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Sign In</button>    
+            </form>
+        </div>
+        <div class="card-footer text-center">
+            <p><a href="forgot-password.html">Forgot your password?</a></p>
+            <p>Don't have an account? <a href="signup.html">Sign up here</a></p>
+        </div>    
+    </div>
+        
     
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="assets/js/bootstrap.min.js"></script>
