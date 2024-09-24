a3df2aa66bb3ea2645cd879314f26c8d30026361
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-09-24 15:40:52 +0400

    rewrited some detials

--- signin.php
+++ signin.php
@@ -10,6 +10,18 @@
     <link rel="icon" href="images/cropped_logo.png" type="image/x-icon">
 </head>
 <body>
+    <header>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
     
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
